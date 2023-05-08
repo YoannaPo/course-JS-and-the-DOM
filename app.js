3d42e183d5e80a1dc31ac204be51f29f9e83a280
YoannaPo <yoannna.pol@gmail.com>
2023-05-08 17:39:46 +0100

    Add performance info

--- app.js
+++ app.js
@@ -228,4 +228,160 @@ document.removeEventListener('click', function myEventListeningFunction() {
 //Imagine a scenario where you see two houses that look exactly the same. 
 //While the houses might look the same, their addresses are different! 
 
+//The difference between capturing and bubbling
+el.addEventListener('click', function () {
+    console.log('You clicked on the 2nd quizzing-quizzes item!');
+}, false);
+//at the end it should be, true --> to be executed as a capturing phase 
+//(if not the default phase is bubbling)
+//Remember that the third argument of the .addEventListener() method is the boolean 
+//for the capturing phase. Since it's false, that means the event listener should
+// not run during the capturing phase...therefore it will run in the (default) bubble phase.
 
+
+// 💡 LISTENER WITH PARAMETER FUMNCTION TO STORE event object: An "event" is an "evt" is an "e" 💡
+
+// Remember that a function's parameter is just like a regular variable. 
+//In the following example, I'm using a parameter with the name event.
+
+const items = document.querySelectorAll('.quizzing-quizzes');
+const el = items[1];
+
+el.addEventListener('keypress', function (event) {
+    console.log('You clicked on the 2nd quizzing-quizzes item!');
+});
+
+//Prevent from default action:
+// Without the event object, we're stuck with the default actions. 
+// However, the event object has a .preventDefault() method on it that a handler can call to prevent 
+// the default action from occurring!
+
+const links = document.querySelectorAll('a');
+const thirdLink = links[2];
+
+thirdLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log("Look, ma! We didn't navigate to a new page!");
+});
+
+
+//Checking the Node Type in Event Delegation
+//Nasz element, ktory chcemy osluchac zostal upchniety w spanie w article, oto jka sie do niego dostac
+// <article id="content">
+//   <p>Brownie lollipop <span>carrot cake</span> gummies lemon drops sweet roll dessert tiramisu. Pudding muffin <span>cotton candy</span> croissant fruitcake tootsie roll. Jelly jujubes brownie. Marshmallow jujubes topping sugar plum jelly jujubes chocolate.</p>
+
+//   <p>Tart bonbon soufflé gummi bears. Donut marshmallow <span>gingerbread cupcake</span> macaroon jujubes muffin. Soufflé candy caramels tootsie roll powder sweet roll brownie <span>apple pie</span> gummies. Fruitcake danish chocolate tootsie roll macaroon.</p>
+// </article>
+
+// In this filler text, notice that there are some <span> tags. 
+//If we want to listen to the <article> for a click on a <span>, you need to add
+//.nodeName for that element (it MUST be written in CAPIAL letters)
+document.querySelector('#content').addEventListener('click', function (evt) {
+    if (evt.target.nodeName === 'SPAN') {  // ← verifies target is desired element
+        console.log('A span was clicked with text ' + evt.target.textContent);
+    }
+});
+//One of the properties of the Node Interface that is inherited is .nodeName. 
+//We can use this property to verify that the target element is actually the element we're looking for. When a <span> element is clicked, it will have a .nodeName property of "SPAN", so the check will pass and the message will be logged. However, if a <p> element is clicked, it will have a .nodeName property of "P",
+// so the check will fail and the message will not be logged.
+
+// The nodeName's Capitalization ⚠️
+
+// The .nodeName property will return an uppercase string, not a lowercase one. So when you perform your check make sure to either:
+
+//     check for capital letters
+//     convert the .nodeName to lowercase
+
+// check using capital letters
+if (evt.target.nodeName === 'SPAN') {
+    console.log('A span was clicked with text ' + evt.target.textContent);
+}
+
+// convert nodeName to lowercase
+if (evt.target.nodeName.toLowerCase() === 'span') {
+    console.log('A span was clicked with text ' + evt.target.textContent);
+}
+
+// Event Delegation is the process of delegating to a parent element the ability to manage events for child elements. We were able to do this by making use of:
+
+//     the event object and its .target property
+//     the different phases of an event
+
+
+
+//Using the DOMContentLoaded Event
+//Because we now know about the DOMContentLoaded event, we can use it to keep our JS code in the <head>.
+//HOw to use it so the Js file will be executed after DOM will be parsed
+
+// Let's update the previous HTML code to include this event:
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <link rel="stylesheet" href="/css/styles.css" />
+//     <script>
+//       document.addEventListener('DOMContentLoaded', function () {
+//           document.querySelector('footer').style.backgroundColor = 'purple';
+//       });
+//     </script>
+//Pretty cool, right?!? We have the JavaScript code in the <head> element, 
+//but it is now wrapped in an event listener for the DOMContentLoaded event. 
+//This will prevent the DOM-styling code from running when the browser gets to it. 
+//Then, when the DOM has been constructed, the event will fire and this code will run.
+
+
+//When would you want to use this technique? 
+//Well, JavaScript code in the <head> will run before JavaScript code in the <body>, 
+//so if you do have JavaScript code that needs to run as soon as possible, 
+//then you could put that code in the <head> and wrap it in a DOMContentLoaded event listener. 
+//This way it will run as early as possible, but not too early that the DOM isn't ready for it.
+
+//Testing if our code is efficient 
+//MEASURE THE SPEED OF YOUR CODE:
+
+const startingTime = performance.now();
+
+for (let i = 1; i <= 100; i++) { 
+  for (let j = 1; j <= 100; j++) {
+    console.log('i and j are ', i, j);
+  }
+}
+
+const endingTime = performance.now();
+console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
+//These are the steps to use performance.now() to measure the speed of your code:
+
+// use performance.now() to get the initial start time for the code - HERE const startingTime
+
+// run the code you want to test
+// execute performance.now() to get another time measurement- HERE const endingTime
+// subtract the initial time from the final time
+
+//What is reflow?
+//Reflow is the process of the browser laying out the page. It happens when you first display the DOM 
+//(generally after the DOM and CSS have been loaded), 
+//and happens again every time something could change the layout. This is a fairly expensive (slow) process.
+//Why are we doing this? The browser is constantly working to make the screen match the DOM. 
+//When we add a new element, the browser has to run through a reflow calculation (to determine the new screen layout) and then repaint the screen. This takes time.
+
+//If we had added each new paragraph to the body element, 
+//then the code would've been a lot slower, because this would cause the browser to go through the reflow and 
+//repaint process for each paragraph. We really only want the browser to do this once, 
+//so we need to attach each new paragraph to something, 
+//but we don't want to have an extra, unneeded element to get added to the DOM.
+
+//Here's  DocumentFragment comes to help - it's like creating another lightweight DOM tree. 
+//But the beneficial part of this is what it says next: changes made to a DocumentFragment 
+//happen off-screen; there's no reflow and repaint cost while you build this. So this is exactly what we need!
+//We can use the .createDocumentFragment() method to create an empty DocumentFragment object.
+
+const fragment = document.createDocumentFragment();  // ← uses a DocumentFragment instead of a <div>
+
+for (let i = 0; i < 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.innerText = 'This is paragraph number ' + i;
+
+    fragment.appendChild(newElement);
+}
+
+document.body.appendChild(fragment); // reflow and repaint here -- once!
